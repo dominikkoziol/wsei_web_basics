00a0aa9e674ec9dc3e1e8c38cd8ba5ba210181d6
Dominik Kozioł <dkoziol097@gmail.com>
2020-03-15 15:31:03 +0100

    add reload pages by JS function

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,7 @@
 let noteArray = (localStorage.getItem("Note") ? JSON.parse(localStorage.getItem("Note")) : []);
+let currentPage = localStorage.getItem("currentPage") ? localStorage.getItem("currentPage") : "home";
+const contentSection = document.getElementById("content");
+loadPage(currentPage);
 
 /**
  * @name - Name of note
@@ -26,4 +29,27 @@ function addNote() {
     console.log(noteArray);
 
     localStorage.setItem("Note", JSON.stringify(noteArray));
+}
+
+function loadPage(href) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", href + '.html', false);
+    xmlhttp.send();
+    var element = document.getElementById(currentPage);
+    if (element) element.parentNode.removeChild(element);
+
+    var newElement = document.createElement("div");
+    newElement.setAttribute("id", href);
+
+    newElement.innerHTML = xmlhttp.responseText;
+    contentSection.appendChild(newElement);
+
+
+    var oldMenuElement = document.getElementById(currentPage + '-link');
+    var newMenuElement = document.getElementById(href + '-link');
+    oldMenuElement.removeAttribute("class");
+    newMenuElement.classList.add("active");
+
+    currentPage = href;
+    localStorage.setItem("currentPage", currentPage);
 }
